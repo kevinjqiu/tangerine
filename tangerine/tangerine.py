f63aae48aab0a89834cddba6649eccfabd0d7679
Kevin Jing Qiu <kevin@idempotent.ca>
2017-11-02 12:13:44 -0400

    Refactoring

--- tangerine/tangerine.py
+++ tangerine/tangerine.py
@@ -1,16 +1,15 @@
 from urllib.parse import urlencode
+import contextlib
 import requests
 
 
 DEFAULT_LOCALE = 'en_CA'
 
 
-class TangerineClient(object):
-    def __init__(self, secret_provider, session=None, locale=DEFAULT_LOCALE):
-        if session is None:
-            session = requests.Session()
-        self.session = session
+class TangerineLoginFlow(object):
+    def __init__(self, secret_provider, session, locale):
         self.secret_provider = secret_provider
+        self.session = session
         self.locale = locale
 
     @staticmethod
@@ -18,15 +17,11 @@ class TangerineClient(object):
         return 'https://secure.tangerine.ca/web/InitialTangerine.html?{}'.format(urlencode(kvs))
 
     @staticmethod
-    def _tangerine(**kvs):
+    def _tangerine_url(**kvs):
         if kvs:
             return 'https://secure.tangerine.ca/web/Tangerine.html?{}'.format(urlencode(kvs))
         else:
             return 'https://secure.tangerine.ca/web/Tangerine.html'
-
-    @staticmethod
-    def _api(path):
-        return 'https://secure.tangerine.ca/web/rest{}'.format(path)
 
     def _get_init_tangerine(self, command):
         return self.session.get(self._init_tangerine_url(command=command, device='web', locale=self.locale),
@@ -39,7 +34,7 @@ class TangerineClient(object):
             'device': 'web',
         })
         resp = self.session.post(
-            self._tangerine(),
+            self._tangerine_url(),
             headers={
                 'x-web-flavour': 'fbe',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -50,20 +45,16 @@ class TangerineClient(object):
         return resp
 
     def _get_tangerine(self, **kv):
-        r = self.session.get(self._tangerine(**kv),
+        r = self.session.get(self._tangerine_url(**kv),
                              headers={'x-web-flavour': 'fbe', 'Accept': 'application/json'})
         r.raise_for_status()
         return r
 
-    def my_account(self):
-        r = self.session.get(self._api('/v1/customers/my'))
-        return r.json()
-
-    def logout(self):
+    def end(self):
         self._get_init_tangerine('displayLogout')
 
-    def login(self):
-        self.logout()
+    def start(self):
+        self._get_init_tangerine('displayLogout')
         self._get_init_tangerine('displayLoginRegular')
 
         self._post_tangerine(data={
@@ -95,5 +86,30 @@ class TangerineClient(object):
         r = self._get_tangerine(command='PINPADPersonal')
         print(r.text)
         r = self._get_tangerine(command='displayAccountSummary', fill=1)
-        # r = self.session.get('https://secure.tangerine.ca/web/Tangerine.html?command=displayAccountSummary&fill=1')
         print(r.text)
+
+
+class TangerineClient(object):
+    def __init__(self, secret_provider, session=None, locale=DEFAULT_LOCALE):
+        if session is None:
+            session = requests.Session()
+        self.session = session
+        self.login_flow = TangerineLoginFlow(secret_provider, self.session, locale)
+
+    @staticmethod
+    def _api(path):
+        return 'https://secure.tangerine.ca/web/rest{}'.format(path)
+
+    @contextlib.contextmanager
+    def login(self):
+        self.login_flow.start()
+        try:
+            yield
+        except:
+            raise
+        finally:
+            self.login_flow.end()
+
+    def my_account(self):
+        r = self.session.get(self._api('/v1/customers/my'))
+        return r.json()
