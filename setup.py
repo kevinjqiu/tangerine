dad17059a16b9ec7189d2755f03780b02cd60308
Kevin Jing Qiu <kevin@idempotent.ca>
2017-11-02 11:46:27 -0400

    Refactoring

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ setuptools.setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
--- tangerine/tangerine.py
+++ tangerine/tangerine.py
@@ -2,15 +2,19 @@ from urllib.parse import urlencode
 import requests
 
 
+DEFAULT_LOCALE = 'en_CA'
+
+
 class TangerineClient(object):
-    def __init__(self, secret_provider, session=None):
+    def __init__(self, secret_provider, session=None, locale=DEFAULT_LOCALE):
         if session is None:
             session = requests.Session()
         self.session = session
         self.secret_provider = secret_provider
+        self.locale = locale
 
     @staticmethod
-    def _init_tangerine(**kvs):
+    def _init_tangerine_url(**kvs):
         return 'https://secure.tangerine.ca/web/InitialTangerine.html?{}'.format(urlencode(kvs))
 
     @staticmethod
@@ -24,68 +28,72 @@ class TangerineClient(object):
     def _api(path):
         return 'https://secure.tangerine.ca/web/rest{}'.format(path)
 
-    def my_account(self):
-        r = self.session.get(self._api('/v1/customers/my'))
-        return r.json()
+    def _get_init_tangerine(self, command):
+        return self.session.get(self._init_tangerine_url(command=command, device='web', locale=self.locale),
+                                headers={'x-web-flavour': 'fbe'})
 
-    def logout(self):
-        self.session.get(self._init_tangerine(command='displayLogout', device='web', locale='en_CA'),
-                         headers={'x-web-flavour': 'fbe'})
-
-    def login(self):
-        self.logout()
-        self.session.get(self._init_tangerine(command='displayLoginRegular', device='web', locale='en_CA',
-                         headers={'x-web-flavour': 'fbe'}))
-
-        r = self.session.post(
-            self._tangerine(),
-            headers={
-                'x-web-flavour': 'fbe',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            },
-            data={
-                'command': 'PersonalCIF',
-                'locale': 'en_CA',
-                'device': 'web',
-                'ACN': self.secret_provider.get_username(),
-            })
-        r.raise_for_status()
-
-        r = self.session.get(self._tangerine(command='displayChallengeQuestion'), headers={'x-web-flavour': 'fbe'})
-        r.raise_for_status()
-
-        question = r.json()['MessageBody']['Question']
-        answer = self.secret_provider.get_security_challenge_answer(question)
-
-        r = self.session.post(
-            self._tangerine(),
-            headers={
-                'x-web-flavour': 'fbe',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            },
-            data={
-                'command': 'verifyChallengeQuestion',
-                'BUTTON': 'Next',
-                'Answer': answer,
-                'Next': 'Next',
-            })
-        r.raise_for_status()
-
-        r = self.session.post(
+    def _post_tangerine(self, data):
+        data = dict(data)
+        data.update({
+            'locale': self.locale,
+            'device': 'web',
+        })
+        resp = self.session.post(
             self._tangerine(),
             headers={
                 'x-web-flavour': 'fbe',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'Accept': 'application/json',
             },
-            data={
-                'locale': 'en_CA',
-                'command': 'validatePINCommand',
-                'BUTTON': 'Go',
-                'PIN': self.secret_provider.get_password(),
-                'Go': 'Next',
-                'callSource': '4',
-            })
+            data=data)
+        resp.raise_for_status()
+        return resp
+
+    def _get_tangerine(self, **kv):
+        r = self.session.get(self._tangerine(**kv),
+                             headers={'x-web-flavour': 'fbe', 'Accept': 'application/json'})
+        r.raise_for_status()
+        return r
+
+    def my_account(self):
+        r = self.session.get(self._api('/v1/customers/my'))
+        return r.json()
+
+    def logout(self):
+        self._get_init_tangerine('displayLogout')
+
+    def login(self):
+        self.logout()
+        self._get_init_tangerine('displayLoginRegular')
+
+        self._post_tangerine(data={
+            'command': 'PersonalCIF',
+            'ACN': self.secret_provider.get_username(),
+        })
+
+        r = self._get_tangerine(command='displayChallengeQuestion')
+        question = r.json()['MessageBody']['Question']
+        answer = self.secret_provider.get_security_challenge_answer(question)
+
+        r = self._post_tangerine(data={
+            'command': 'verifyChallengeQuestion',
+            'BUTTON': 'Next',
+            'Answer': answer,
+            'Next': 'Next',
+        })
+
+        r = self._post_tangerine(data={
+            'locale': self.locale,
+            'command': 'validatePINCommand',
+            'BUTTON': 'Go',
+            'PIN': self.secret_provider.get_password(),
+            'Go': 'Next',
+            'callSource': '4',
+        })
+
         print(r.text)
-        r = self.session.get(self._tangerine(command='PINPADPersonal'))
+        r = self._get_tangerine(command='PINPADPersonal')
+        print(r.text)
+        r = self._get_tangerine(command='displayAccountSummary', fill=1)
+        # r = self.session.get('https://secure.tangerine.ca/web/Tangerine.html?command=displayAccountSummary&fill=1')
         print(r.text)
