41991a7aa624a8b8c1721ee15a2e27c2eea4ccfe
Kevin Jing Qiu <kevin@idempotent.ca>
2017-11-03 20:57:41 -0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tangerine",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/kevinjqiu/tangerine",
 
     author="Kevin J. Qiu",
